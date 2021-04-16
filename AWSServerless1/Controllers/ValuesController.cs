9b68dab2a30b95c5aba9476acf57a66f5a275031
sarwe100 <krishnasarwe@gmail.com>
2021-04-16 17:51:51 +0530

    made changes

--- AWSServerless1/Controllers/ValuesController.cs
+++ AWSServerless1/Controllers/ValuesController.cs
@@ -17,7 +17,7 @@ namespace AWSServerless1.Controllers
         }
         public IEnumerable<string> GetALl()
         {
-            return new string[] { "cc", "value2", "AWS Serverless", "Azur Functions" };
+            return new string[] { "kk", "value2", "AWS Serverless", "Azur Functions" };
         }
 
         // GET api/values/5
